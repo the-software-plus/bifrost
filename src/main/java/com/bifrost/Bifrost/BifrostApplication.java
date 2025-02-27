7cfbb0ab46afd8715f93160546b6a8192f4f816a
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2025-02-26 21:51:20 -0300

    project start

---
+++ src/main/java/com/bifrost/Bifrost/BifrostApplication.java
@@ -0,0 +1,13 @@
+package com.bifrost.Bifrost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BifrostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BifrostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bifrost/Bifrost/BifrostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bifrost.Bifrost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BifrostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
