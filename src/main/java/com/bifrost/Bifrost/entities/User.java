4959bf6fde8b3b168c54da4e799f6eecb8aebbce
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2025-03-31 22:19:45 -0300

    Criando a entidade de usuário

---
+++ src/main/java/com/bifrost/Bifrost/entities/User.java
@@ -0,0 +1,52 @@
+package com.bifrost.Bifrost.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(unique = true)
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    public enum Values {
+        ADMIN(1L),
+        BASIC(2L);
+
+        Long roleId;
+
+        Values(Long roleId) {
+            this.roleId = roleId;
+        }
+
+        public Long getRoleId() {
+            return roleId;
+        }
+    }
+}
