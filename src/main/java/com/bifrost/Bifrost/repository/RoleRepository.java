d1c48266efea7abf7ad2b2023380054e32800ea4
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2025-03-31 22:25:58 -0300

    Criando Repositories

---
+++ src/main/java/com/bifrost/Bifrost/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.bifrost.Bifrost.repository;
+
+import com.bifrost.Bifrost.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/bifrost/Bifrost/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.bifrost.Bifrost.repository;
+
+import com.bifrost.Bifrost.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
