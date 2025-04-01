5c00150856ca0324fe3602df7e351df91eb068c4
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2025-03-31 22:20:45 -0300

    Criando a entidade de role

---
+++ src/main/java/com/bifrost/Bifrost/entities/Role.java
@@ -0,0 +1,21 @@
+package com.bifrost.Bifrost.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String name;
+}
